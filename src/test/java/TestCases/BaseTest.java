9547aad53fd46088525d04e30e4e4886c8610771
AbstractaThreadsTeam <threads_automation@abstracta.com.uy>
2022-06-02 23:22:24 -0300

    after class

--- src/test/java/TestCases/BaseTest.java
+++ src/test/java/TestCases/BaseTest.java
@@ -5,6 +5,7 @@ import Helpers.Utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 
@@ -35,7 +36,7 @@ public class BaseTest {
 
     }
 
-    @AfterMethod
+    @AfterClass
     public void after() {
         try {
             if (driver != null) {
